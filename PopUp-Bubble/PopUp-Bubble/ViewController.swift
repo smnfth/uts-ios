22a46c3146cbcfb1014d2749b6e5c0a671d0ae8b
Samaneh Fathieh <samanehfathieh@Samanehs-MacBook-Pro.local>
2018-05-05 12:23:41 +1000

    button added

--- PopUp-Bubble/PopUp-Bubble/ViewController.swift
+++ PopUp-Bubble/PopUp-Bubble/ViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func bubbleTapped(<#parameters#>) -> <#return type#> {
+        <#function body#>
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let bubbleImage = UIImage.init(imageLiteralResourceName: "pink.jpg")
+        let bubbleView = UIImageView(image: bubbleImage)
+        bubbleView.frame = CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0)
+        let tapHandler = UITapGestureRecognizer(target: self, action: #selector(bubbleTapped(_:)))
+        bubbleView.addGestureRecognizer(tapHandler)
+        
+        self.view.addSubview(bubbleView)
     }
+    
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
