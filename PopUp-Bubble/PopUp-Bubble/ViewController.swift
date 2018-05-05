ac5713a6925fd274ab6ae4710bab160064289196
alifa20 <ali.fathieh@gmail.com>
2018-05-05 15:07:09 +1000

    random colors added

--- PopUp-Bubble/PopUp-Bubble/ViewController.swift
+++ PopUp-Bubble/PopUp-Bubble/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
         return Timer.scheduledTimer(timeInterval: delay, target: BlockOperation(block: block), selector: #selector(Operation.main), userInfo: nil, repeats: false)
     }
     
+    let probablities = [UIColor.black, UIColor.blue, UIColor.blue, UIColor.green, UIColor.green, UIColor.green, UIColor.purple, UIColor.purple, UIColor.purple, UIColor.purple, UIColor.purple, UIColor.purple, UIColor.red, UIColor.red, UIColor.red, UIColor.red, UIColor.red, UIColor.red, UIColor.red, UIColor.red];
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -33,7 +34,9 @@ class ViewController: UIViewController {
         //        let randomY = CGFloat(randomSource.nextUniform()) * (self.view.frame.height-100)
         let button = MyButton(frame: CGRect(x: x, y: y, width: 100, height: 100))
         
-        button.backgroundColor = .green
+//        button.backgroundColor = .green
+        let randomIndex = Int(arc4random_uniform(UInt32(probablities.count)))
+        button.backgroundColor = probablities[randomIndex]
         button.setTitle("Test Button", for: .normal)
         button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
         
